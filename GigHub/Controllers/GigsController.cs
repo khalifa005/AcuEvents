80fc85a277ce80cff287b0a0d5a8c55c95420d14
mahmoud khalifa <mahmudkhalifa12@outlook.com>
2018-05-03 01:10:58 +0200

    Add Post Action For AddGig

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,5 +1,7 @@
 ﻿using GigHub.Models;
 using GigHub.ViewModel;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -19,7 +21,7 @@ namespace GigHub.Controllers
             return View();
         }
 
-
+        [Authorize]
         public ActionResult Create()
         {
 
@@ -28,6 +30,28 @@ namespace GigHub.Controllers
                 Genres = _context.Genres.ToList()
             };
             return View(ViewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create (GigsViewModel ViewModel)
+        {
+            var ArtistId = User.Identity.GetUserId();
+            var Artist = _context.Users.Single(u => u.Id == ArtistId);
+
+            var GenreId = _context.Genres.Single(g => g.Id == ViewModel.Genre);
+            var gig = new Gig
+            {
+                Artist = Artist,
+                Venue = ViewModel.Venue,
+                Genre = GenreId,
+                DateTime = DateTime.Parse(string.Format("{0} {1}", ViewModel.Date, ViewModel.Time))
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
